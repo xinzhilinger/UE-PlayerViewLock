d2fc7422c7850cf78ec421d1cec1136f14843575
xinzhilinger <58837333+xinzhilinger@users.noreply.github.com>
2022-04-13 14:39:02 +0800

    Delete DoubleHitProject.cpp

--- DoubleHitProject/DoubleHitProject.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "DoubleHitProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DoubleHitProject, "DoubleHitProject" );
